87343dd6334ca6417ca8833b444f4f01636fedab
Brittany <brittany@fundera.com>
2018-09-09 14:10:30 -0400

    added single view and create new

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,24 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
   end
 
   def create
-    render plain: params[:restaurant].inspect
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to @restaurant
   end
+
+  private
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :description)
+    end
+
 end
